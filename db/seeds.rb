e48590f005c2cbb7d9eaae7420a72e5db59527eb
Hirandil <eredoine@gmail.com>
2014-03-07 12:29:25 +0100

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Annonce.create(title: 'Vend Galaxy S3 OP telephone', description: 'Téléphone trés bonne état, peu utilisé',price: 150,url: 'http://img1.lesnumeriques.com/produits/36/12749/samsung-galaxy-s3.jpg')
+Annonce.create(title: 'Viture trés bien', description: 'Trés belle ferrari tas vu',price: 200000,url: 'http://hypercars.fr/wp-content/uploads/2014/01/Ferrari-GTE-Concept-by-Angelo-Granata-__-Hypercars-__-6.jpg')
