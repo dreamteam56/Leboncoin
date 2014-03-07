cf20a99a799f32a90231e834381c79d83ffc6142
abrantes01 <abrantes.kevin@gmail.com>
2014-03-07 09:45:35 +0100

    initialisation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leboncoin::Application.config.secret_key_base = 'd60089e9a7177dde76608880f25e3d29ff22559a0fa88991c437ac6dc5e5b04546972517aab9b3750e04b6f493319fe9d1842b55ee250c50430e08bf5b8f4cac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Leboncoin::Application.config.session_store :cookie_store, key: '_Leboncoin_session'
