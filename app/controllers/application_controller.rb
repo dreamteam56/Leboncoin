3f825df45eaa6d5e987f2b942f3a572eec28b7ac
abrantes01 <abrantes.kevin@gmail.com>
2014-03-08 01:15:07 +0100

    Debut CSS sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,6 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  def avatar_url(user)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
-  end
-
   protected
 
   def configure_permitted_parameters
