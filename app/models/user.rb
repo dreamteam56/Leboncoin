47903012668617df3b31102a148a314ec1b9e549
Hirandil <eredoine@gmail.com>
2014-03-11 14:21:35 +0100

    Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, {
+    :styles => {
+      :thumb => ["50x50#", :png],
+      :medium => ["100x100#", :png],
+      :large => ["300x300>", :png]
+    },
+    :convert_options => {
+      :thumb => "-gravity Center -crop 50x50+0+0",
+      :thumb => "-gravity Center -crop 100x100+0+0",
+    }
+  }
 end
