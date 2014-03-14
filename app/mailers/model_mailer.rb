2564471fe5db1b8eceda4f7275a45b49178eba84
abrantes01 <abrantes.kevin@gmail.com>
2014-03-14 16:14:14 +0100

    show

--- app/mailers/model_mailer.rb
+++ app/mailers/model_mailer.rb
@@ -1,5 +1,5 @@
 class ModelMailer < ActionMailer::Base
-  default from: "me@leboncoin.com"
+  default from: "eredoine@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
