788a79b658b6f858982293636f650671ed20772d
abrantes01 <abrantes.kevin@gmail.com>
2014-04-07 15:38:28 +0200

    Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,19 @@ Leboncoin::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "sandbox21042.mailgun.org",
+    :user_name => "postmaster@sandbox21042.mailgun.org",
+    :password => "1jmvdgwk4gj5"
+  }
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
