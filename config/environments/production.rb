5fa4169cc01b5509e4e8b1dd10f13689df1295a4
abrantes01 <abrantes.kevin@gmail.com>
2014-04-07 14:36:12 +0200

    Las version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Leboncoin::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.initialize_on_precompile = true
+  
 
   # Generate digests for assets URLs.
   config.assets.digest = true
